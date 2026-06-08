0d2510c6fb26eaead3d6532c133efbabf53a9934
Sejal <sejalfarkade205@gmail.com>
2026-06-08 21:37:57 +0530

    streamlit setup+homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+
+def footer_home():
+
+    st.markdown(f"""
+        <div style="margin-top:2rem;display:flex;gap:6px;justify-content:center; items-align:center">
+                <p style="font-weight:bold; color:white;">&copy; 2026 Sejal | </p>
+                <p style="font-weight:bold; color:white;">
+                Connect:
+                <a href="https://github.com/sejalfarkade29" target="_blank">GitHub</a>|
+                <a href="https://www.linkedin.com/in/sejal-farkade-bb4911289" target="_blank">LinkedIn</a>|
+                <a href="sejalfarkade205@gmail.com">Email</a>
+                </P>
+        </div>
+                """,unsafe_allow_html=True)
