03171c1fd333474c0b2c068df78456085fdb0fbb
Sejal <sejalfarkade205@gmail.com>
2026-07-08 00:30:04 +0530

    Use environment variables for Supabase

--- src/database/config.py
+++ src/database/config.py
@@ -1,20 +1,7 @@
-import streamlit as st
+import os
 from supabase import create_client, Client
+
 supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
+    os.environ["SUPABASE_URL"],
+    os.environ["SUPABASE_KEY"]
 )
-
-
-
-#import streamlit as st
-#from supabase import create_client
-
-#st.write("URL:", st.secrets["SUPABASE_URL"])
-#st.write("KEY starts with:", st.secrets["SUPABASE_KEY"][:20])
-
-#supabase = create_client(
-#    st.secrets["SUPABASE_URL"],
-#    st.secrets["SUPABASE_KEY"]
-#)
-
